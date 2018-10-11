07f353a33b38ee2514916bb0401ae76859f92583
marcos-x86 <marcos.x86@outlook.com>
2018-10-11 14:34:39 -0400

    Base64 encoder initial implementation

--- src/SFVBolivia/Helpers/SFVBoliviaHelper.cs
+++ src/SFVBolivia/Helpers/SFVBoliviaHelper.cs
@@ -16,8 +16,22 @@ namespace SFVBolivia.Helpers
         {
         }
 
-        public void GetBase64()
+        public string GetBase64(int number)
         {
+            char[] charsetBase64 = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
+                                    'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                                    'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                                    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+                                    'y', 'z', '+', '/'};
+            int quotient = number;
+            StringBuilder encodedResult = new StringBuilder();
+            do
+            {
+                encodedResult.Insert(0, charsetBase64[quotient % 64]);
+                quotient = quotient / 64;
+            }
+            while (quotient > 0);
+            return encodedResult.ToString();
         }
 
         public void GetQRCode()
--- src/SFVBoliviaTest/SFVBoliviaHelperTest.cs
+++ src/SFVBoliviaTest/SFVBoliviaHelperTest.cs
@@ -1,14 +1,44 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SFVBolivia.Helpers;
 
 namespace SFVBoliviaTest
 {
     [TestClass]
     public class SFVBoliviaHelperTest
     {
+        SFVBoliviaHelper helper = new SFVBoliviaHelper();
+
         [TestMethod]
-        public void TestMethod1()
+        public void GetBase64EncodedStringOfZeroValueTest()
         {
+            string generatedResult = helper.GetBase64(0);
+            string expectedResult = "0";
+            Assert.AreEqual(expectedResult, generatedResult);
+        }
+
+        [TestMethod]
+        public void GetBase64EncodedStringOfNineDigitsNumberOneTest()
+        {
+            string generatedResult = helper.GetBase64(934598591);
+            string expectedResult = "tjDU/";
+            Assert.AreEqual(expectedResult, generatedResult);
+        }
+
+        [TestMethod]
+        public void GetBase64EncodedStringOfNineDigitsNumberTwoTest()
+        {
+            string generatedResult = helper.GetBase64(434376710);
+            string expectedResult = "Pv106";
+            Assert.AreEqual(expectedResult, generatedResult);
+        }
+
+        [TestMethod]
+        public void GetBase64EncodedStringOfNineDigitsNumberThreeTest()
+        {
+            string generatedResult = helper.GetBase64(204986118);
+            string expectedResult = "CDzS6";
+            Assert.AreEqual(expectedResult, generatedResult);
         }
     }
 }
