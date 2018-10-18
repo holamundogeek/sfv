fa7335ea41102a1d50228c858a4e867e1331d9ee
brunofvm <bruno.vasquez@jalasoft.com>
2018-10-18 16:54:29 -0400

    RC4 message cipher initial implementation

--- src/SFVBolivia/Helpers/SFVBoliviaHelper.cs
+++ src/SFVBolivia/Helpers/SFVBoliviaHelper.cs
@@ -12,8 +12,44 @@ namespace SFVBolivia.Helpers
         {
         }
 
-        public void GetRC4Ciphertext()
+        public String GetRC4Ciphertext(String message, String key)
         {
+            int[] state = Enumerable.Range(0, 256).ToArray();
+            return  MessageCipher(message, KeyCipher(key, state));
+        }
+
+        private int[] KeyCipher(string key, int[] state)
+        {
+            int index1 = 0, index2 = 0;
+            foreach (int i in Enumerable.Range(0, 256)) {
+                index2 = (key[index1] + state[i] + index2) % 256;
+                state = Swap(state, index2, i);
+                index1 = (index1 + 1) % key.Length;
+            }
+            return state;
+        }
+
+        private String MessageCipher(string message, int[] state)
+        {
+            int index1 = 0, index2 = 0;
+            String messageCipher = "";
+            foreach (int i in Enumerable.Range(0, message.Length))
+            {
+                index1 = (index1 + 1) % 256;
+                index2 = (state[index1] + index2) % 256;
+                state = Swap(state, index1, index2);
+                string nMen = (message[i] ^ state[(state[index1] + state[index2]) % 256]).ToString("X");
+                messageCipher = messageCipher + "-" + (nMen.Length == 1 ? "0" + nMen : nMen);
+            }
+            return messageCipher.Substring(1, messageCipher.Length - 1);
+        }
+
+        private int[] Swap(int[] state, int index1, int index2)
+        {
+            int temp = state[index2];
+            state[index2] = state[index1];
+            state[index1] = temp;
+            return state;
         }
 
         public void GetBase64()
--- src/SFVBoliviaTest/SFVBoliviaHelperTest.cs
+++ src/SFVBoliviaTest/SFVBoliviaHelperTest.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SFVBolivia.Helpers;
 
 namespace SFVBoliviaTest
 {
     [TestClass]
     public class SFVBoliviaHelperTest
     {
+        SFVBoliviaHelper helper = new SFVBoliviaHelper();
+
         [TestMethod]
-        public void TestMethod1()
+        public void GetRC4CiphertextTest()
         {
+            String actualMessageCiffer = helper.GetRC4Ciphertext("d3Ir6", "sesamo");
+            String expectedMessageCiffer = "EB-06-AE-F8-92";
+            Assert.AreEqual(expectedMessageCiffer, actualMessageCiffer);
+        }
+
+        [TestMethod]
+        public void GetRC4CiphertextLongPasswordTest()
+        {
+            String actualMessageCiffer = helper.GetRC4Ciphertext("IUKYo", "XBCPY-GKGX4-PGK44-8B632-X9P33");
+            String expectedMessageCiffer = "83-62-FC-B0-F0";
+            Assert.AreEqual(expectedMessageCiffer, actualMessageCiffer);
+        }
+
+        [TestMethod]
+        public void GetRC4CiphertextComplexPasswordTest()
+        {
+            String actualMessageCiffer = helper.GetRC4Ciphertext("piWCp", "Aa1-bb2-Cc3-Dd4");
+            String expectedMessageCiffer = "37-71-2E-14-A0";
+            Assert.AreEqual(expectedMessageCiffer, actualMessageCiffer);
         }
     }
 }
